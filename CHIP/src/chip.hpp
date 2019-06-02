cfdd4a6a825495bdedb861ffbff09b94974c887a
Motherborn <yoda.master.91@gmail.com>
2019-06-02 18:39:26 +0300

    Implemented opcodes

--- CHIP/src/chip.hpp
+++ CHIP/src/chip.hpp
@@ -36,69 +36,69 @@ public:
 
 	void step()
 	{
-		//test 00e0
-		memory[pc] = 0x00;
-		memory[pc + 1] = 0xe0;
+		////test 00e0
+		//memory[pc] = 0x00;
+		//memory[pc + 1] = 0xe0;
 
-		//test 00ee
-		memory[pc] = 0x00;
-		memory[pc + 1] = 0xee;
+		////test 00ee
+		//memory[pc] = 0x00;
+		//memory[pc + 1] = 0xee;
 
-		//test 1000
-		memory[pc] = 0x12;
-		memory[pc + 1] = 0x14;
+		////test 1000
+		//memory[pc] = 0x12;
+		//memory[pc + 1] = 0x14;
 
-		//test 2000
-		memory[pc] = 0x23;
-		memory[pc + 1] = 0x26;
+		////test 2000
+		//memory[pc] = 0x23;
+		//memory[pc + 1] = 0x26;
 
-		//test 3000
-		memory[pc] = 0x30;
-		memory[pc + 1] = 0x00;
+		////test 3000
+		//memory[pc] = 0x30;
+		//memory[pc + 1] = 0x00;
 
-		//test 4000
-		memory[pc] = 0x4a;
-		memory[pc + 1] = 0x00;
+		////test 4000
+		//memory[pc] = 0x4a;
+		//memory[pc + 1] = 0x00;
 
-		//test 6000
-		memory[pc] = 0x6b;
-		memory[pc + 1] = 0x04;
+		////test 6000
+		//memory[pc] = 0x6b;
+		//memory[pc + 1] = 0x04;
 
-		//test 7000
-		memory[pc] = 0x7d;
-		memory[pc + 1] = 0x01;
+		////test 7000
+		//memory[pc] = 0x7d;
+		//memory[pc + 1] = 0x01;
 
-		//test 8xy0
-		memory[pc] = 0x88;
-		memory[pc + 1] = 0xd0;
+		////test 8xy0
+		//memory[pc] = 0x88;
+		//memory[pc + 1] = 0xd0;
 
-		//test a000
-		memory[pc] = 0xa3;
-		memory[pc + 1] = 0x62;
+		////test a000
+		//memory[pc] = 0xa3;
+		//memory[pc + 1] = 0x62;
 
-		//test d000
-		memory[pc] = 0xd8;
-		memory[pc + 1] = 0x91;
+		////test d000
+		//memory[pc] = 0xd8;
+		//memory[pc + 1] = 0x91;
 
-		//test exa1
-		memory[pc] = 0xe0;
-		memory[pc + 1] = 0xa1;
+		////test exa1
+		//memory[pc] = 0xe0;
+		//memory[pc + 1] = 0xa1;
 
-		//test fx07
-		memory[pc] = 0xf0;
-		memory[pc + 1] = 0x07;
+		////test fx07
+		//memory[pc] = 0xf0;
+		//memory[pc + 1] = 0x07;
 
-		//test fx15
-		memory[pc] = 0xf0;
-		memory[pc + 1] = 0x15;
+		////test fx15
+		//memory[pc] = 0xf0;
+		//memory[pc + 1] = 0x15;
 
-		//test fx18
-		memory[pc] = 0xf1;
-		memory[pc + 1] = 0x18;
+		////test fx18
+		//memory[pc] = 0xf1;
+		//memory[pc + 1] = 0x18;
 
-		//test fx29
-		memory[pc] = 0xfc;
-		memory[pc + 1] = 0x29;
+		////test fx29
+		//memory[pc] = 0xfc;
+		//memory[pc + 1] = 0x29;
 
 		//test ff80 ???
 		//test e010 ???
@@ -110,11 +110,59 @@ public:
 
 		//decode
 		switch (opcode & 0xF000) {
-		case 0x2000: // Call subroutine at nnn. (2nnn - CALL addr)
+		case 0x0000:
+		{
+			switch (opcode & 0x00FF) {
+			case 0x00E0: // Clear the display. (00E0 - CLS)
+			{
+				//Clear display
+			}
+			case 0x00EE: // Return from a subroutine. (00EE - RET) ready
+			{
+				pc = stack[sp];
+				sp--;
+
+				std::cout << "[OPCODE 0x00EE] pc: 0x" << pc << " | dec: " << std::dec << pc << " | sp: " << sp << std::endl;
+			}
+			default:
+				std::cerr << "[OPCODE 0x0000]: Cannot find specified opcode: " << opcode << std::endl;
+				break;
+			}
+		}
+		case 0x1000: // Jump to location nnn. (1nnn - JP addr) ready
+		{
+			pc = opcode & 0x0FFF;
+			std::cout << "[OPCODE 0x1000] pc: 0x" << pc << " | dec: " << std::dec << pc << std::endl;
+		}
+		case 0x2000: // Call subroutine at nnn. (2nnn - CALL addr) ready
 		{
 			stack[sp] = pc;
 			pc = opcode & 0x0FFF;
 			sp++;
+		}
+		case 0x3000: // Skip next instruction if Vx = kk. (3xkk - SE Vx, byte)
+		{
+			int x = opcode & 0x0F00 >> 8;
+			int kk = opcode & 0x00FF;
+
+			if (V[x] == kk) {
+				pc += 4;
+			} else {
+				pc += 2;
+			}
+		}
+		case 0x4000: // Skip next instruction if Vx != kk. (4xkk - SNE Vx, byte)
+		{
+			int x = opcode & 0x0F00 >> 8;
+			int kk = opcode & 0x00FF;
+
+			std::cout << "[OPCODE 0x4000] x: 0x" << x << " | 0x: " << kk << " | Vx: " << V[x] << std::endl;
+
+			if (V[x] != kk) {
+				pc += 4;
+			} else {
+				pc += 2;
+			}
 		}
 		case 0x5000: // Skip next instruction if Vx = Vy. (5xy0 - SE Vx, Vy)
 		{
@@ -124,9 +172,13 @@ public:
 			std::cout << "[OPCODE 0x5000] x: 0x" << x << " | dec: " << std::dec << x << std::endl;
 			std::cout << "[OPCODE 0x5000] y: 0x" << y << " | dec: " << std::dec << y << std::endl;
 
-
+			if (V[x] == V[y]) {
+				pc += 4;
+			} else {
+				pc += 2;
+			}
 		}
-		case 0x6000: // The interpreter puts the value kk into register Vx. (6xkk - LD Vx, byte) ready
+		case 0x6000: // Set Vx = kk. (6xkk - LD Vx, byte) ready
 		{
 			int x = (opcode & 0x0F00) >> 8;
 			int nn = (opcode & 0x00FF);
@@ -135,6 +187,28 @@ public:
 			std::cout << "[OPCODE 0x6000] nn: 0x" << nn << " | dec: " << std::dec << nn << std::endl;
 			
 			V[x] = nn;
+			pc += 2;
+			break;
+		}
+		case 0x7000: // Set Vx = Vx + kk. (7xkk - ADD Vx, byte) ready
+		{
+			int x = (opcode & 0x0F00) >> 8;
+			int nn = (opcode & 0x00FF);
+
+			std::cout << "[OPCODE 0x7000] x: 0x" << x << " | dec: " << std::dec << x << std::endl;
+			std::cout << "[OPCODE 0x7000] nn: 0x" << nn << " | dec: " << std::dec << nn << std::endl;
+
+			V[x] += nn;
+			pc += 2;
+			break;
+		}
+		case 0xA000: //The value of register I is set to nnn. (Annn - LD I, addr) ready
+		{
+			int nnn = (opcode & 0x0FFF);
+
+			std::cout << "[OPCODE 0xA000] nnn: 0x" << nnn << " | dec: " << std::dec << nnn << std::endl;
+
+			I = nnn;
 			pc += 2;
 			break;
 		}
